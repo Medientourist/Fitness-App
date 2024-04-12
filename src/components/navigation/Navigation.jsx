db3bc011a1c9e776ecee67499efa528cb0acefd8
Malte <medientourist@gmail.com>
2024-04-12 21:06:13 +0200

    some changes

---
+++ src/components/navigation/Navigation.jsx
@@ -0,0 +1,19 @@
+function Navigation() {
+  return (
+    <div className="">
+      <nav className="flex justify-around space-x-8">
+        <div className="inline">
+          <p className="inline">Hello 1</p>
+        </div>
+        <div className="inline">
+          <p className="inline">Hello 2</p>
+        </div>
+        <div className="inline">
+          <p className="inline">Hello 3</p>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navigation;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   content: ["./src/index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: "#202430",
+        medium: "#3A4151",
+        light: "#FBFBFB",
+        gradiant1: "#FBFBFB",
+        gradiant2: "#FBFBFB",
+        gradiant3: "#FBFBFB",
+      },
+      fontFamily: {
+        standard: ["Poppins"],
+      },
+    },
   },
   plugins: [],
 };
