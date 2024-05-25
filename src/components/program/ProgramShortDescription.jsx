b24e06832a3d90badbf3f148fd843f885b2dcf5b
Malte <medientourist@gmail.com>
2024-05-25 15:29:48 +0200

    Prototyp for timer

--- src/components/program/ProgramShortDescription.jsx
+++ src/components/program/ProgramShortDescription.jsx
@@ -1,9 +1,15 @@
+import { Link } from "react-router-dom";
+import close from "../../assets/close.png";
+
 function ProgramShortDescription() {
   return (
     <div className="h-screen flex flex-col justify-center items-center text-center bg-gradient-light-orange px-4">
       <div>
         <h1>Titel des Programms</h1>
       </div>
+      <Link to="/workout" className="absolute top-4 right-4">
+        <img src={close} alt="back" className="w-6 h-6" />
+      </Link>
       <div className="absolute bottom-0 w-full">
         <div className="flex justify-around mb-4">
           <div className="text-center">
--- src/components/training/TrainingStart.jsx
+++ src/components/training/TrainingStart.jsx
@@ -1,21 +1,27 @@
 import { Link } from "react-router-dom";
 import back from "../../assets/back.png";
 import ProgramStartButton from "../program/ProgramStartButton";
+import TrainingTimer from "./TrainingTimer";
 
 function Training() {
   return (
-    <div className="bg-dark min-h-screen flex flex-col text-center">
-      <div className="w-full flex justify-center items-center relative">
-        <h1 className="text-base">Titel des Programms</h1>
-        <Link to="/programs" className="absolute right-4">
-          <img src={back} alt="back" className="w-6 h-6" />
-        </Link>
+    <div>
+      <div className="bg-dark min-h-screen flex flex-col text-center">
+        <div className="w-full flex justify-center items-center relative">
+          <h1 className="text-base">Titel des Programms</h1>
+          <Link to="/programs" className="absolute right-4">
+            <img src={back} alt="back" className="w-6 h-6" />
+          </Link>
+        </div>
+        <div className="flex flex-col justify-center items-center flex-grow">
+          <h2>Tag 1</h2>
+          <p>26 Min. • Kraft und Koordination</p>
+        </div>
+        <ProgramStartButton />
       </div>
-      <div className="flex flex-col justify-center items-center flex-grow">
-        <h2>Tag 1</h2>
-        <p>26 Min. • Kraft und Koordination</p>
+      <div className="">
+        <TrainingTimer />
       </div>
-      <ProgramStartButton />
     </div>
   );
 }
