64b7cb7f58d508215fa733031fa1ad5593442ffc
Malte <medientourist@gmail.com>
2024-05-28 09:40:54 +0200

    deleted unecessary console.logs in colorWheel

--- src/components/overview/colorWheel.js
+++ src/components/overview/colorWheel.js
@@ -11,9 +11,7 @@ let lastIndex = -1;
 // Funktion, die eine Klasse aus der Liste zurückgibt
 function getNextClass() {
   // Inkrementiere den Index
-  console.log(lastIndex);
   lastIndex = (lastIndex + 1) % classNamesList.length;
-  console.log(lastIndex);
   // Gib die nächste Klasse zurück
   return classNamesList[lastIndex];
 }
