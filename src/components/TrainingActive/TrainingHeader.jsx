d88dddc88b0a77fbeee345e3885e9a20f23b6be4
Medientourist <medientourist@gmail.com>
2024-08-07 10:29:23 +0200

    TrainingStop finished

--- src/components/TrainingActive/TrainingHeader.jsx
+++ src/components/TrainingActive/TrainingHeader.jsx
@@ -1,9 +1,10 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useQuery } from "@apollo/client";
+import TrainingStop from "./TrainingStop";
 import { GET_WORKOUT } from "../../queries/hygraphQueries";
-import { useParams, useLocation } from "react-router-dom";
-import { useQuery, gql } from "@apollo/client";
 
 function TrainingHeader({ programId, workoutId, day, style }) {
+  const [isClicked, setIsClicked] = useState(false);
   const { loading, error, data } = useQuery(GET_WORKOUT, {
     variables: { id: workoutId },
   });
@@ -13,19 +14,38 @@ function TrainingHeader({ programId, workoutId, day, style }) {
 
   const { workout } = data;
 
+  const handleClick = () => {
+    setIsClicked(true);
+  };
+
+  const handleClose = () => {
+    setIsClicked(false);
+  };
+
   return (
-    <div className="text-center mt-2.5">
+    <div className="text-center mt-2.5 relative">
       <div className="flex items-center justify-center">
         <p className="text-white">{workout.name}</p>
-        <Link
-          to={`/program/${programId}?workoutId=${encodeURIComponent(
-            workoutId
-          )}&day=${encodeURIComponent(day)}&style=${encodeURIComponent(style)}`}
-          className="text-white absolute right-4"
-        >
-          <img src="../../assets/close.png" alt="stop" className="w-6 h-6" />
-        </Link>
+        <img
+          src="../../assets/close.png"
+          alt="stop"
+          className="cursor-pointer absolute right-4"
+          onClick={handleClick}
+        />
       </div>
+      {isClicked && (
+        <>
+          <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-10">
+            <TrainingStop
+              programId={programId}
+              workoutId={workoutId}
+              day={day}
+              style={style}
+              onClose={handleClose}
+            />
+          </div>
+        </>
+      )}
     </div>
   );
 }
--- src/components/TrainingActive/TrainingStop.jsx
+++ src/components/TrainingActive/TrainingStop.jsx
@@ -1,14 +1,21 @@
-function TrainingStop() {
+import { Link } from "react-router-dom";
+
+function TrainingStop({ programId, workoutId, day, style, onClose }) {
   return (
-    <div className="bg-medium text-center">
+    <div className="bg-medium text-center p-4 rounded-xl shadow-lg">
       <p>Möchtest du das Workout wirklich beenden?</p>
-      <div className="flex justify-around">
-        <button className="bg-medium p-2 mx-2 rounded-3xl">
+      <div className="flex justify-around mt-4">
+        <button className="bg-medium p-2 mx-2 rounded-3xl" onClick={onClose}>
           Nein, weiter machen!
         </button>
-        <button className="bg-black px-4 py-2 mx-2 rounded-3xl">
+        <Link
+          to={`/program/${programId}?workoutId=${encodeURIComponent(
+            workoutId
+          )}&day=${encodeURIComponent(day)}&style=${encodeURIComponent(style)}`}
+          className="bg-black px-4 py-2 mx-2 rounded-3xl text-white"
+        >
           Ja, beenden
-        </button>
+        </Link>
       </div>
     </div>
   );
